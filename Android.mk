b8dc1d4e4b567ecbb3495d73c0d8d0090e51b83f
XO <jyotiraditya182@gmail.com>
2018-07-12 11:02:00 -0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-msm8996)
+
 sdm-libs := sdm/libs
 display-hals := include libqservice libqdutils $(sdm-libs)/utils $(sdm-libs)/core
 
@@ -21,4 +23,6 @@ else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
 endif
+endif
+
 endif
